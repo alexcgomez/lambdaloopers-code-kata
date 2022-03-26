7db6e4792eb7780b69c554e3f12a786062f517d5
Alex Cáceres <alexcaceres93@gmail.com>
2022-03-26 13:47:18 +0100

    Working on Spare. Test in red

--- src/game.js
+++ src/game.js
@@ -1,12 +1,26 @@
 class Game {
     currentScore;
+    currentFrame;
+    spare;
 
     constructor() {
         this.currentScore = 0;
+        this.currentFrame = 1;
+        this.spare = false;
     }
 
     roll(pins) {
+        if (this.currentFrame === 2) {
+            this.currentFrame = 1;
+        } else {
+            this.currentFrame += 1;
+        }
+
         this.currentScore += pins;
+    }
+
+    score() {
+        return this.currentScore;
     }
 }
 
--- test/game.test.js
+++ test/game.test.js
@@ -1,11 +1,36 @@
 const Game = require('../src/game')
 
-const game = new Game();
 
-test('current score should be the sum of the accumulated rolls', () => {
-    const expectedCurrentScore = 5;
-    game.roll(2);
-    game.roll(0);
-    game.roll(3);
-    expect(game.currentScore).toBe(expectedCurrentScore);
+describe('Game', () => {
+
+    let game;
+
+    beforeEach(() => {
+        game = new Game();
+        jest.setMock()
+    })
+
+    test('current score should be the sum of the accumulated rolls', () => {
+        const expectedCurrentScore = 5;
+        game.roll(2);
+        game.roll(0);
+        game.roll(3);
+        expect(game.currentScore).toBe(expectedCurrentScore);
+    })
+
+    test('score method should return the current score', () => {
+        const expectedTotalScore = 8;
+        game.roll(5);
+        game.roll(3);
+        expect(game.score()).toBe(expectedTotalScore)
+    })
+
+    test('should accumulate next roll score if spare',  () => {
+        const expectedCurrentScore = 26;
+        game.roll(2);
+        game.roll(8);
+        game.roll(8);
+        expect(game.score()).toBe(expectedCurrentScore)
+    })
+
 })
